0d1d18495f24500e583f0ce2195156086e886d61
kalejos <ksalejos@ksu.edu>
2017-07-10 18:51:11 -0500

    Add files via upload

---
+++ Lab03a-Testing/lab03a_function4.hpp
@@ -0,0 +1,70 @@
+#ifndef _function4
+#define _function4
+
+#include <string>
+using namespace std;
+
+int GetLength(string word)
+{
+	int length = word.size();
+    word.size();
+    return length;
+}
+
+void Test_GetLength()
+{
+    cout << "************ Test_GetLength ************" << endl;
+
+    string input;
+    int expectedOutput;
+    int actualOutput;
+
+    /* TEST 1 ********************************************/
+    input = "cat";
+    expectedOutput = 3;
+
+    actualOutput = GetLength( input );
+    if ( actualOutput == expectedOutput )
+    {
+        cout << "Test_GetLength: Test 1 passed!" << endl;
+    }
+    else
+    {
+        cout << "Test_GetLength: Test 1 FAILED!" << endl;
+    }
+
+    /* TEST 2 ********************************************/
+    // CREATE YOUR OWN TEST
+    input = "book";             // change me
+    expectedOutput = 4;     // change me
+
+    actualOutput = GetLength( input );
+    if ( actualOutput == expectedOutput )
+    {
+        cout << "Test_GetLength: Test 1 passed!" << endl;
+    }
+    else
+    {
+        cout << "Test_GetLength: Test 1 FAILED!" << endl;
+    }
+
+    /* TEST 3 ********************************************/
+    // CREATE YOUR OWN TEST
+    input = "A";             // change me
+    expectedOutput = 1;     // change me
+
+    actualOutput = GetLength( input );
+    if ( actualOutput == expectedOutput )
+    {
+        cout << "Test_GetLength: Test 1 passed!" << endl;
+    }
+    else
+    {
+        cout << "Test_GetLength: Test 1 FAILED!" << endl;
+    }
+}
+
+#endif
+
+
+
